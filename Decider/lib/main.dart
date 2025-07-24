071b0f5e0a2fbb1241a40da6e45e66d6d66a7e42
Lemuel Abellana <labellana_230000002074@uic.edu.ph>
2025-07-24 11:36:22 +0800

    Add files via upload
    Abellana Decider

---
+++ Decider/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:decider/views/home_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Decider App',
+      theme: ThemeData(
+        primaryColor: Colors.red,
+
+        ),
+      home: HomeView(),
+    );
+  }
+}
